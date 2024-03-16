18e7644ceba60caf37e9ec8f98fc1dda38e0794b
uchzmal <117048798+uchzmal@users.noreply.github.com>
2024-03-16 16:35:31 +0800

    Update 第5期-SVIP专属超级主题【QQ主题】.sh

--- 第5期-SVIP专属超级主题【QQ主题】.sh
+++ 第5期-SVIP专属超级主题【QQ主题】.sh
@@ -3,12 +3,14 @@
 # 打印作者信息
 echo "思路和资源 by 欲归来南枝 @https://space.bilibili.com/1495315743"
 echo "脚本制作 by zmal/梦断忆- @https://space.bilibili.com/554419861"
+echo "root用户请保证mt管理器有root权限，非root用户请保证开启QQ目录（储存器）api调用的权限"
+sleep 5
 
-# 创建目录
+# 创建临时缓存目录
 mkdir -p 0
 
 # 使用curl下载文件
-curl -O https://github.com/uchzmal/QQ-theme-replacement/raw/main/%E7%AC%AC5%E6%9C%9F-SVIP%E4%B8%93%E5%B1%9E%E8%B6%85%E7%BA%A7%E4%B8%BB%E9%A2%98%E3%80%90QQ%E4%B8%BB%E9%A2%98%E3%80%91.zip 
+curl -O https://github.com/uchzmal/QQ-theme-replacement/raw/main/%E7%AC%AC5%E6%9C%9F-SVIP%E4%B8%93%E5%B1%9E%E8%B6%85%E7%BA%A7%E4%B8%BB%E9%A2%98%E3%80%90QQ%E4%B8%BB%E9%A2%98%E3%80%91.zip  
 
 # 检查curl是否成功执行，如果成功则执行解压命令和删除压缩文件的命令
 if [ $? -eq 0 ]; then
