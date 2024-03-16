f7ba7d96fe5f161c5759ca4d29d8dddd68b4cf5a
uchzmal <117048798+uchzmal@users.noreply.github.com>
2024-03-16 14:39:17 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -9,20 +9,22 @@ sleep 5
 theme_dir="/data/user/0/com.tencent.mobileqq/app_theme_810/"
 
 # 列出目录中的文件夹，排除名称为1001的文件夹，获取非1001的数字文件夹数量
-flies=$(ls -l "$theme_dir" | grep -E '^d' | grep -v '1001' | wc -l)
+files=$(ls -l "$theme_dir" | grep -E '^d' | grep -v '1001' | wc -l) 
 
 # 循环遍历非1001的数字文件夹
-for ((i=1; i<=$flies; i++)); do
+for ((i=1; i<=$files; i++)); do
     # 获取第i个非1001的数字文件夹的绝对路径
-    m=$(ls -d "$theme_dir"* | grep -v '1001' | sed -n ${i}p)
+    folder=$(ls -d "$theme_dir"* | grep -v '1001' | sed -n ${i}p)
     # 获取第i个非1001的数字文件夹的名称
-    f=$(basename "$m")
-    # 替换资源文件，假设源文件为 /path/to/source_file
-    cp -r "./0/1" "$m/theme.${f}"
+    folder_name=$(basename "$folder")
+    # 获取第i个非1001的数字文件夹内文件夹名称
+    subfolder_name=$(ls "$folder") 
+    # 替换资源文件
+    cp -r "./0/1/ab4099a947e80fac9fc3f33952ac3680" "$folder/theme.${folder_name}/$subfolder_name"
     # 拷贝压缩文件到文件夹中，并重命名
-    cp "./0/2.zip" "$m/theme.${f}.zip"
+    cp "./0/2.zip" "$folder/theme.${folder_name}.zip"
     # 修改文件夹权限
-    chmod -R 555 "$theme_dir" 
+    chmod -R 555 "$theme_dir"
 done
 
 # 删除缓存，假设要删除的文件夹在脚本执行的当前目录下
