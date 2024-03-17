ca4b301655dfae1b0168cd543aaedf3ce8105ed5
uchzmal <117048798+uchzmal@users.noreply.github.com>
2024-03-17 17:44:03 +0800

    Add files via upload

--- user.sh
+++ user.sh
@@ -25,12 +25,12 @@ for ((i=1; i<=$files; i++)); do
     # 获取第i个非1001的数字文件夹内文件夹theme.${folder_name}内文件夹名称
     subfolder_name=$(find "$folder/theme.${folder_name}" -mindepth 1 -maxdepth 1 -type d -printf "%f\n")
     # 替换资源文件
-    cp -r "./0/1" "$folder/theme.${folder_name}/$subfolder_name"
+    cp -r "./zmal.theme.temp/1" "$folder/theme.${folder_name}/$subfolder_name"
     # 拷贝压缩文件到文件夹中，并重命名
-    cp "./0/2.zip" "$folder/theme.${folder_name}.zip"
+    cp "./zmal.theme.temp/2.zip" "$folder/theme.${folder_name}.zip"
     # 修改文件夹权限
     chmod -R 555 "$theme_dir"
 done
 
 # 删除缓存，假设要删除的文件夹在脚本执行的当前目录下
-rm -rf "./0"
+rm -rf "./zmal.theme.temp"
--- 第5期-SVIP专属超级主题【QQ主题】.sh
+++ 第5期-SVIP专属超级主题【QQ主题】.sh
@@ -4,17 +4,17 @@
 echo "思路和资源 by 欲归来南枝 @https://space.bilibili.com/1495315743"
 echo "脚本制作 by zmal/梦断忆- @https://space.bilibili.com/554419861"
 echo "root用户请保证mt管理器有root权限，非root用户请保证开启QQ目录（储存器）api调用的权限"
-sleep 5
+#sleep 5
 
 # 创建临时缓存目录
-mkdir -p 0
+mkdir -p zmal.theme.temp
 
 # 使用curl下载文件
-curl -O https://github.com/uchzmal/QQ-theme-replacement/raw/main/%E7%AC%AC5%E6%9C%9F-SVIP%E4%B8%93%E5%B1%9E%E8%B6%85%E7%BA%A7%E4%B8%BB%E9%A2%98%E3%80%90QQ%E4%B8%BB%E9%A2%98%E3%80%91.zip theme.zip
+curl -O theme.zip https://raw.githubusercontent.com/uchzmal/QQ-theme-replacement/main/%E7%AC%AC5%E6%9C%9F-SVIP%E4%B8%93%E5%B1%9E%E8%B6%85%E7%BA%A7%E4%B8%BB%E9%A2%98%E3%80%90QQ%E4%B8%BB%E9%A2%98%E3%80%91.zip
 
 # 检查curl是否成功执行，如果成功则执行解压命令和删除压缩文件的命令
 if [ $? -eq 0 ]; then
-    unzip "theme.zip" -d 0 && rm -rf "theme.zip"
+    unzip "theme.zip" -d zmal.theme.temp && rm -rf "theme.zip"
 else
     echo "下载文件失败"
     exit 1
@@ -27,8 +27,8 @@ if [ $? -ne 0 ]; then
 fi
 
 # 移动解压后的文件并重命名，确保命令间没有不可见的特殊字符
-mv "./0/2028656/theme.2028656/ab4099a947e80fac9fc3f33952ac3680" "./0/1" &&
-mv "./0/2028656/theme.2028656.zip" "./0/2.zip" 
+mv "./zmal.theme.temp/2028656/theme.2028656/ab4099a947e80fac9fc3f33952ac3680" "./zmal.theme.temp/1" &&
+mv "./zmal.theme.temp/2028656/theme.2028656.zip" "./zmal.theme.temp/2.zip" 
 
 # 输出信息，告知用户操作已经完成
 echo "初始化完成"
@@ -36,7 +36,7 @@ echo "初始化完成"
 # 检查当前用户是否为root
 if [ $(id -u) -eq 0 ]; then
     # 如果是root用户，则执行root.sh
-    curl -sL https://github.com/uchzmal/QQ-theme-replacement/raw/main/root.sh | bash
+    su curl -sL https://github.com/uchzmal/QQ-theme-replacement/raw/main/root.sh | bash
    
 else
     # 如果不是root用户，则执行user.sh
