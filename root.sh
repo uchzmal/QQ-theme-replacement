edf1c1c547e451d1d92452f35a0aa76308a08e10
uchzmal <117048798+uchzmal@users.noreply.github.com>
2024-03-19 23:04:04 +0800

    更新 root.sh

--- root.sh
+++ root.sh
@@ -2,33 +2,41 @@
 
 # 打印作者信息
 echo "思路和资源 by 欲归来南枝 @https://space.bilibili.com/1495315743"
-echo "脚本制作 by zmal/梦断忆- @https://space.bilibili.com/554419861" 
+echo "脚本制作 by zmal/梦断忆- @https://space.bilibili.com/554419861"
 
 # 定义目录路径
-theme_dir="/data/user/0/com.tencent.mobileqq/app_theme_810/" 
-#初始化权限
-su chmod -R 777 "$theme_dir" 
-echo 开始替换
+theme_dir="/data/user/0/com.tencent.mobileqq/app_theme_810/"
+
+# 初始化权限
+chmod -R 777 "$theme_dir"
+echo "开始替换"
+
 # 列出目录中的文件夹，排除名称为1001的文件夹，获取非1001的数字文件夹数量
-files=$(su ls -l "$theme_dir" | grep -E '^d' | grep -v '1001' | wc -l) 
+files=$(ls -l "$theme_dir" | grep -E '^d' | grep -v '1001' | wc -l)
 
 # 循环遍历非1001的数字文件夹
 for ((i=1; i<=$files; i++)); do
-    echo $i
+    echo "处理第 $i 个文件夹"
+    
     # 获取第i个非1001的数字文件夹的绝对路径
-    folder=$(su ls -d "$theme_dir"* | grep -v '1001' | sed -n ${i}p) 
+    folder=$(ls -d "$theme_dir"* | grep -v '1001' | sed -n "${i}p")
+    
     # 获取第i个非1001的数字文件夹的名称
-    folder_name=$(su basename "$folder")
+    folder_name=$(basename "$folder")
+    
     # 获取第i个非1001的数字文件夹内文件夹theme.${folder_name}内文件夹名称
-    subfolder_name=$(su find "$folder/theme.${folder_name}" -mindepth 1 -maxdepth 1 -type d -printf "%f\n")
+    subfolder_name=$(find "$folder/theme.${folder_name}" -mindepth 1 -maxdepth 1 -type d -printf "%f\n")
+    
     # 替换资源文件
-    su cp -r "./zmal.theme.temp/1" "$folder/theme.${folder_name}/$subfolder_name" 
+    cp -r "./zmal.theme.temp/1" "$folder/theme.${folder_name}/$subfolder_name"
+    
     # 拷贝压缩文件到文件夹中，并重命名
-    su cp "./zmal.theme.temp/2.zip" "$folder/theme.${folder_name}.zip"
+    cp "./zmal.theme.temp/2.zip" "$folder/theme.${folder_name}.zip"
+    
     # 修改文件夹权限
-    su chmod -R 555 "$theme_dir" 
+    chmod -R 555 "$theme_dir"
 done
 
-# 删除缓存，假设要删除的文件夹在脚本执行的当前目录下
+# 删除缓存
 rm -rf "./zmal.theme.temp"
-echo 替换结束
+echo "替换结束"
